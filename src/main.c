89812700d188f7af97b7f8a1652ccdc9fcaecea8
Jupe987 <jpajungas24@gmail.com>
2025-11-22 16:06:08 +0200

    update 9 minor bug fixes and re formatting

--- src/main.c
+++ src/main.c
@@ -16,6 +16,8 @@ char MORSE_CHAR  = 'a';
 float gravity = 0.95;
 char message[64];
 int count = 0;
+float ax, ay, az, gx, gy, gz, t;
+
 
 enum state { WAITING=1, DATA_READY, ENDOFMESG };
 enum state programState = WAITING;
@@ -35,13 +37,13 @@ void display_morse(){
 
 void read_usb(){
     
-    int ch = getchar_timeout_us(1000);
+    int ch = getchar_timeout_us(200);
     if (ch >= 0) {
         if(ch == '.' || ch == '-' || ch == ' ') {
             MORSE_CHAR = ch;
             programState = DATA_READY;            
         }
-        if((char)ch == '\n'){
+        if(ch == 'n'){
             programState = ENDOFMESG;
         }
     }
@@ -62,32 +64,101 @@ void play_buzzer(){
         buzzer_play_tone(5000, 300);
     } 
 }
+void positon_handler(){
+
+    if(az > 0.9 && ay < 0.2 && ax < 0.2) {
+        programState = DATA_READY;
+        MORSE_CHAR = '.';
+    }
+    else if(az < 0.2 && ay > 0.9 && ax < 0.2){
+        programState = DATA_READY;
+        MORSE_CHAR = '-';
+    }
+    else if(az < 0.2 && ay < 0.2 && ax > 0.9){
+        programState = DATA_READY;
+        MORSE_CHAR = ' ';
+    }
+    else{
+        programState = WAITING;
+    }
+    
+}
+void motion_handler(){
+    //intialise variales to store sum of acc and reset them each call
+    float ax_sum = 0.0, ay_sum = 0.0, az_sum = 0.0;
+    float ax_grav = 0.0, ay_grav = 0.0, az_grav = 0.0;
+    //HOW MAMY SAMPLES
+    int sample_size = 10;
+    while(sample_size > 0){
+        ax_grav = ax - gravity;
+        ay_grav = ay - gravity;
+        az_grav = az - gravity;
+        if(ax_grav > 0.0){
+            ax_sum = ax_sum + ax_grav;
+        }
+        else if(ay_grav > 0.0) {
+            ay_sum = ay_sum + ay_grav;
+        }
+        else if(az_grav > 0.0) {
+            az_sum = az_sum + az_grav;
+        }
+
+        sample_size  = sample_size  -1;
+        //Little delay between samples
+        sleep_ms(35);
+        ICM42670_read_sensor_data(&ax, &ay , &az, &gx, &gy, &gz, &t);
+        //printf("%s\n","HERE2");
+    }
+
+    if(az_sum > 3){
+        printf("%0.2f",az_sum);
+        MORSE_CHAR = '.';
+        programState = DATA_READY;
+    }
+
+    if(ax_sum > 2.2){
+        printf("%0.2f",ax_sum);
+        MORSE_CHAR = '-';
+        programState = DATA_READY;
+    }
+}
 
 static void print_task(void *arg){
+    //HANDLES THE USB MESSAGE GETTING AND PRINTING AND STORING THE MESSAGE 
+    //ALSO BUZZER AND DISPLAY 
     (void)arg;
     puts("USB ready to recive symbols");
     //intianaliense the display
     init_display();
     clear_display();
+    init_buzzer();
 
     for(;;){
-        if(programState == ENDOFMESG){
-        putchar_raw((char) '\n');
-        puts("message was:");
-        puts(message);
-        puts("new message");
 
-        programState = WAITING;
-        }
         if(programState == WAITING) {
             read_usb();
         }
-        // IF WE HAVE DATA 
+        if(programState == ENDOFMESG){
+            //HANDLES THE END OF MESSAGE
+            putchar_raw((char) '\n');
+            puts("message was:");
+            puts(message);
+            puts("new message:");
+            count = 0;
+            clear_display();
+            write_text("msg recived");
+
+            programState = WAITING;
+        }
+        // IF WE HAVE DATA THAT WE NEED TO STORE AND PRINT IT TO PC
         if(programState == DATA_READY) {
 
             display_morse();
             play_buzzer();
+            //Store the full message
             message[count] = (char)MORSE_CHAR;
+            count++;
+            message[count] = ' ';
             count++;
             message[count] = '\0';
      
@@ -98,91 +169,42 @@ static void print_task(void *arg){
             programState = WAITING;
         }
 
-        vTaskDelay(pdMS_TO_TICKS(2000));
+        vTaskDelay(pdMS_TO_TICKS(1500));
     }
 }
+
 static void sensor_task(void *arg){
-    // This task reads data from sensor and based on data we get the morse char
+    // This task reads data from sensor and based on data we get the corresponding morse char
     (void)arg;
     init_ICM42670();
-    float ax, ay, az, gx, gy, gz, t;
-
     
     if(ICM42670_start_with_default_values() != 0){
         printf("%s\n","gyro failed to start");
     }
+    //LED TO INDICATE WHEN DATA COLLECTION IS HAPPENING
+    init_led();
 
     for(;;){
-        //intialise variales to store sum of acc and reset them
-        float ax_sum = 0.0, ay_sum = 0.0, az_sum = 0.0;
-        float ax_grav, ay_grav, az_grav = 0.0;
-
+        
         // Read sensor
         ICM42670_read_sensor_data(&ax, &ay , &az, &gx, &gy, &gz, &t);
        
         if (gpio_get(SW2_PIN) == 1 && programState == WAITING) {
-            // indicate that we are ready to move board
+            // indicate that user can move the board 
             toggle_led();
+            //human reaction time
             sleep_ms(200);
 
-            //How many readings we want
-            int sample_size = 10;
-            while(sample_size > 0){
-                ax_grav = ax - gravity;
-                ay_grav = ay - gravity;
-                az_grav = az - gravity;
-                if(ax_grav > 0.0){
-                    ax_sum = ax_sum + ax_grav;
-                }
-                else if(ay_grav > 0.0) {
-                    ay_sum = ay_sum + ay_grav;
-                }
-                else if(az_grav > 0.0) {
-                    az_sum = az_sum + az_grav;
-                }
-                else{
-                    sample_size = sample_size -1;
-                }
-                sample_size  = sample_size  -1;
-                //Little delay between samples
-                sleep_ms(35);
-                ICM42670_read_sensor_data(&ax, &ay , &az, &gx, &gy, &gz, &t);
-                //printf("%s\n","HERE2");
-            }
+            motion_handler();
+
             toggle_led();
 
-            if(az_sum > 3){
-                //printf("%s\n","HERE az sum comp");
-                MORSE_CHAR = '.';
-                programState = DATA_READY;
-            }
-
-            if(ax_sum > 2.2){
-                MORSE_CHAR = '-';
-                programState = DATA_READY;
-            }
-
-            // gets the position
             if(programState != DATA_READY && gpio_get(SW2_PIN) == 1) {
-                if(az > 0.9 && ay < 0.2 && ax < 0.2) {
-                    programState = DATA_READY;
-                    MORSE_CHAR = '.';
-                }
-                else if(az < 0.2 && ay > 0.9 && ax < 0.2){
-                    programState = DATA_READY;
-                    MORSE_CHAR = '-';
-                }
-                else if(az < 0.2 && ay < 0.2 && ax > 0.9){
-                    programState = DATA_READY;
-                    MORSE_CHAR = ' ';
-                }
-                else{
-                    programState = WAITING;
-                }
+                //IF NOT CHAR BASED ON MOVING THEN IT HAS TO BE STILL
+                positon_handler();
             }
         }
-
-        vTaskDelay(pdMS_TO_TICKS(2000));
+        vTaskDelay(pdMS_TO_TICKS(1500));
     }
 }
 
@@ -198,12 +220,11 @@ int main() {
     init_hat_sdk();
 
     sleep_ms(300); //Wait some time so initialization of USB and hat is done.
-
-    init_buzzer();
-    init_led();
-
+    
+    //BUTTONN FOR INTERUPTION
     init_sw1();
     init_sw2();
+
     gpio_set_irq_enabled_with_callback(BUTTON1, SW1_PIN, true, btn_fxn);
     //gpio_set_irq_enabled_with_callback(BUTTON2, SW2_PIN, true, btn2_fxn);
 
